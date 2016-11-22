9d95e63bf3969374ef0a4dd64864c6fd4f5eaccb
lifangda02@gmail.com <lifangda02@gmail.com>
2016-11-21 22:31:42 -0500

    LevMar now works, need to add radial distortion

--- HW09/main.py
+++ HW09/main.py
@@ -1,3 +1,42 @@
-#!/usr/bin/python
-from pylab import *
-import cv2
+import levmar
+import scipy.optimize
+from numpy.testing import *
+import numpy
+
+
+## This is y-data:
+y_data = numpy.array([0.2867, 0.1171, -0.0087, 0.1326, 0.2415, 0.2878, 0.3133, 0.3701, 0.3996, 0.3728, 0.3551, 0.3587, 0.1408, 0.0416, 0.0708, 0.1142, 0, 0, 0])
+
+## This is t-data:
+t = numpy.array([67., 88, 104, 127, 138, 160, 169, 188, 196, 215, 240, 247, 271, 278, 303, 305, 321, 337, 353])
+
+def fitfunc(p, t):
+    """This is the equation"""
+    return p[0] + (p[1] -p[0]) * ((1/(1+numpy.exp(-p[2]*(t-p[3])))) + (1/(1+numpy.exp(p[4]*(t-p[5])))) -1)
+   
+def errfunc(p, t, y):
+    return fitfunc(p,t) -y
+    
+guess = numpy.array([0, max(y_data), 0.1, 140, -0.1, 270])
+p2, C, info, msg, success = scipy.optimize.leastsq(errfunc, guess, args=(t, y_data), full_output=1)
+
+print p2
+
+print numpy.sum(errfunc(p2, t, y_data)**2)
+print numpy.sum(errfunc(p3, t, y_data)**2)
+# p2, C, info, msg, success = scipy.optimize(
+
+    
+#iters, result = levmar.ddif(func, initial, measurement, 5000,
+#                            data = Ab)
+
+# if result:
+#     ex, ey, ez, et = result
+#     tx, ty, tz, tt = map(float, bits[16:20])
+#     print '%.4f %.4f %.4f (%.4f %.4f %.4f) %d' % \
+#           (ex, ey, ez,  tx, ty, tz, iters)
+
+# ##chk = levmar.dchkjac(func, jacf, (0.5, 0.5, 0.5, 10.0), 4, Ab)
+
+            
+        
--- HW09/zhangs.py
+++ HW09/zhangs.py
@@ -2,6 +2,7 @@
 from pylab import *
 import cv2
 import os
+from scipy.optimize import leastsq
 from llsm import get_llsm_homograhpy_from_points
 from corner_detection import extract_sorted_corners
 
@@ -42,12 +43,12 @@ def get_intrinsic_matrix(W):
 		Given the image of absolute conic W,
 		return the intrinsic camera matrix K.
 	'''
-	x0 = (W[0,1]*W[0,2] - W[0,0]*W[1,2]) / (W[0,0]*W[1,1] - W[0,1]**2)
-	lamda = W[2,2] - ( W[0,2]**2 + x0*(W[0,1]*W[0,2] - W[0,0]*W[1,2]) ) / W[0,0]
+	y0 = (W[0,1]*W[0,2] - W[0,0]*W[1,2]) / (W[0,0]*W[1,1] - W[0,1]**2)
+	lamda = W[2,2] - ( W[0,2]**2 + y0*(W[0,1]*W[0,2] - W[0,0]*W[1,2]) ) / W[0,0]
 	ax = sqrt(lamda / W[0,0])
 	ay = sqrt(lamda*W[0,0] / (W[0,0]*W[1,1] - W[0,1]**2))
 	s = - W[0,1]*ax*ax*ay / lamda
-	y0 = s*x0 / ay - W[0,2]*ax*ax / lamda
+	x0 = s*y0 / ay - W[0,2]*ax*ax / lamda
 	K = array([ [ax, s, x0],
 				[0., ay, y0],
 				[0., 0., 1.] ])
@@ -96,10 +97,45 @@ def reproject_corners(P, wcorners):
 		pcorners.append(pcorner)
 	return pcorners
 
+def pack_parameters(K, Rts):
+	'''
+		Given K and Rt matrices for all poses, pack them into a single vector.
+	'''
+	p = K.flatten()[[0,1,2,4,5]]
+	for Rt in Rts:
+		R = Rt[:,:3]
+		r,_ = cv2.Rodrigues(R)
+		r = r.flatten()
+		t = Rt[:,3].flatten()
+		p = hstack((p,r,t))
+	return p
+
+def unpack_parameters(p):
+	'''
+		Given a single vector, unpack into K and Rt matrices.
+	'''
+	K = array([[p[0], p[1], p[2]],
+				[0., p[3], p[4]],
+				[0., 0., 1.]])
+	Rts = []
+	p = p[5:].reshape(-1,6)
+	nRows = p.shape[0]
+	for i in range(nRows):
+		row = p[i,:]
+		R,_ = cv2.Rodrigues(row[:3])
+		t = row[3:].reshape(3,-1)
+		Rt = hstack((R,t))
+		Rts.append(Rt)
+	return K, Rts
+
 def main():
 	N = 40	# number of images
 	d = 25	# size of square in mm
 	wcorners = get_world_frame_corners(d)
+	# Convert list to array, each column is a point
+	arrayWorld = zeros((3,80))
+	arrayImage = zeros((3,80*N))
+	for i,w in enumerate(wcorners): arrayWorld[:,i] = w
 	Hs = []
 	images = []
 	# Compute all homography matrices 
@@ -108,24 +144,46 @@ def main():
 		images.append(image)
 		# Extract corners first
 		icorners = extract_sorted_corners(image)
-		print './Dataset1/Pic_{0}.jpg'.format(i), 'Number of corners found...', len(icorners)
+		arrayImage[:,(i-1)*80 : i*80] = array(icorners).T
+		print 'Number of corners found...', len(icorners)
 		# Compute the homography from world to image plane
 		H = get_llsm_homograhpy_from_points(wcorners, icorners)
 		Hs.append(H)
 	# Extract the intrinsic matrix
 	W = get_absolute_conic_image(Hs)
 	K = get_intrinsic_matrix(W)
-	print "K =", K
+	print "Before LM, K =", K
 	# Extract the extrinsic matrix for each image
 	Rts = []
 	for H in Hs:
 		Rt = get_extrinsic_matrix(K, H)
 		Rts.append(Rt)
-
+	# Refine the K,R,t estimates with Levenberg-Marquedt
+	p_guess = pack_parameters(K, Rts)
+	def error_function(p):
+		'''
+			Geometric distance as cost function for LevMar.
+		'''
+		arrayTrans = zeros(arrayImage.shape)
+		K, Rts = unpack_parameters(p)
+		for i,Rt in enumerate(Rts):
+			P = dot(K, Rt)
+			Hhat = P[:, [0,1,3]]
+			trans = dot(Hhat, arrayWorld)
+			trans = trans / trans[-1,:]
+			arrayTrans[:,i*80 : (i+1)*80] = trans
+		error = arrayTrans - arrayImage
+		return error[:2,:].flatten()
+	print "Optimizing..."
+	p_refined, _ = leastsq(error_function, p_guess)
+	K_refined, Rts_refined = unpack_parameters(p_refined)
+	print "After LM, K_refined =", K_refined
 	# Backproject to validate
-	n = 10
-	Rt = Rts[n]
-	P = dot(K, Rt)
+	n = 0
+	# Rt = Rts[n]
+	# P = dot(K, Rt)
+	Rt = Rts_refined[n]
+	P = dot(K_refined, Rt)
 	print "Rt =", Rt
 	print "P =", P
 	pcorners = reproject_corners(P, wcorners)
