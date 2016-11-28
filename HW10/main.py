8db684c5fda545bc924e274f55ed03dd5081f0ee
Fangda Li <lifangda02@gmail.com>
2016-11-27 19:43:22 -0500

    added rectification

---
+++ HW10/main.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+from pylab import *
+import cv2
+from rectification import *
+
+def pick_points(image):
+	'''
+		Prompt user to click points on image.
+	'''
+	points = []
+	fig = plt.figure()
+	def onclick(event):
+		if len(points) == 8:
+			return
+		x = int(event.xdata)
+		y = int(event.ydata)
+		points.append((x,y))
+		print "Mouse clicked at (x,y)...", (x,y), "; Total number of points clicked...", len(points)
+		print points
+	imshow(image)
+	cid = fig.canvas.mpl_connect('button_press_event', onclick)
+	show()
+
+def main():
+	image1 = imread('./images/1.jpg')
+	image2 = imread('./images/2.jpg')
+	# Uncomment if necessary
+	# pick_points(image1)
+	# pick_points(image2)
+	pts1 = [(92, 112), (290, 142), (131, 263), (245, 296), (151, 326), (151, 365), (101, 493), (277, 428)]
+	pts2 = [(39, 142), (257, 115), (66, 259), (176, 310), (80, 315), (81, 352), (52, 441), (248, 503)]
+	F = get_fundamental_matrix(pts1, pts2)
+	print "F = ", F
+	e, ep = get_epipoles(F)
+	print "e = ", e
+	print "ep = ", ep
+	P, Pp = get_canonical_projection_matrices(F, ep)
+	print "P = ", P
+	print "Pp = ", Pp
+	H, Hp =	get_rectification_homographies(image1, image2, pts1, pts2, e, ep, P, Pp)
+	print "H = ", H
+	print "Hp = ", Hp
+	print "-------- After Rectification --------"
+	print "e = ", dot(H, e)
+	print "ep = ", dot(Hp, ep)
+	
+
+if __name__ == '__main__':
+	main()
---
+++ HW10/rectification.py
@@ -0,0 +1,151 @@
+#!/usr/bin/python
+from pylab import *
+import cv2
+
+def apply_transformation_on_points(points, H):
+	'''
+		Apply the given transformation matrix on all points from input.
+		@coords: list of input points, each is represented by (row,col)
+		@return: list of points after transformation, each is represented by (row,col)
+	'''
+	l = []
+	for point in points:
+		p = array([point[0], point[1], 1.])
+		p = dot(H, p)
+		p = p / p[-1]
+		l.append( (p[0], p[1]) )
+	return l
+
+def solve_homo_system_with_llsm(pts1, pts2):
+	'''
+		Obtain the the solution to a homogeneous linear least square problem.
+		@pts1,pts2: list of matching points
+		@return: ndarray of H		
+	'''
+	n = len(pts1)
+	A = zeros((n,9))
+	for i in range(n):
+		x,y,w = pts1[i][0], pts1[i][1], 1. 
+		xp,yp,wp = pts2[i][0], pts2[i][1], 1. 
+		A[i] = [xp*x, xp*y, xp, yp*x, yp*y, yp, x, y, 1]
+	# Solution is the right eigenvector corresponding to the smallest eigenvalue
+	U, s, Vt = svd( dot(A.T, A) )
+	v = Vt[-1,:]
+	return v
+
+def condition_fundamental_matrix(F):
+	'''
+		Condition the fundamental matrix using SVD such that it's of rank 2.
+	'''
+	U, s, Vt = svd(F)
+	s[-1] = 0.
+	D = diag(s)
+	F = dot( U, dot( D, Vt ) )
+	F = F / F[-1,-1]
+	return F
+
+def get_normalization_homography_matrix():
+	'''
+		Find the homography matrix that transforms the given coordinates to zero-mean 
+	'''
+	pass
+
+def get_right_null_space(A, eps=1e-5):
+	'''
+		Return the null space vector(s) of a matrix.
+	'''
+	U, s, Vt = svd(A)
+	null_space = compress(s <= eps, Vt, axis=0)
+	return null_space.T
+
+def get_cross_product_equiv_matrix(w):
+	'''
+		Get the skew-symmetric cross-product equivalent matrix of a vector
+	'''
+	x,y,z = w[0],w[1],w[2]
+	return array([[0., -z, y],
+				[z, 0., -x],
+				[-y, x, 0.]])
+
+def get_epipoles(F):
+	'''
+		Given fundamental matrix, return the left and right epipole.
+	'''
+	e = get_right_null_space(F)
+	assert e.shape[1] == 1, "More than one left epipoles have been found."
+	ep = get_right_null_space(F.T)
+	assert ep.shape[1] == 1, "More than one right epipoles have been found."
+	return e/e[-1], ep/ep[-1]
+
+def get_canonical_projection_matrices(F, ep):
+	'''
+		Given fundamental matrix and epipole of the right image plane, find both camera projection matrices.
+	'''
+	P = hstack(( eye(3), zeros((3,1)) ))
+	s = get_cross_product_equiv_matrix(ep)
+	Pp = hstack(( dot(s, F), ep ))
+	return P, Pp
+
+def get_fundamental_matrix(pts1, pts2):
+	'''
+		Given point correspondences, make an initial estimate of fundamental matrix F.
+	'''
+	get_normalization_homography_matrix()
+	f = solve_homo_system_with_llsm(pts1, pts2)
+	append(f, 1.)
+	F = f.reshape(3,3)
+	F = condition_fundamental_matrix(F)
+	return F
+
+def get_rectification_homographies(image1, image2, pts1, pts2, e, ep, P, Pp):
+	'''
+		Find the homography matrices that align the corresponding epipolar lines to the same row.
+	'''
+	# Start with the second image first
+	h2, w2 = image2.shape[0], image2.shape[1]
+	# Translational matrix that shifts the image to be origin-centered
+	T1 = array([[1., 0., -w2/2.], 
+				[0., 1., -h2/2.],
+				[0., 0., 1.]])
+	# Rotational matrix that rotates the epipole onto x-axis
+	theta = arctan( (ep[1] - h2/2.) / (ep[0] - w2/2.) )
+	# Since we want to rotate to positive x-axis
+	theta = -theta[0]
+	R = array([[cos(theta), -sin(theta), 0.], 
+				[sin(theta), cos(theta), 0.],
+				[0., 0., 1.]])
+	# Homography that takes epipole to infinity
+	f = norm(array([ep[1] - h2/2., ep[0] - w2/2.]))
+	G = array([[1., 0., 0.], 
+				[0., 1., 0.],
+				[-1./f, 0., 1.]])
+	# Translate back to original center
+	T2 = array([[1., 0., w2/2.], 
+			[0., 1., h2/2.],
+			[0., 0., 1.]])
+	# The final homography for the second image
+	Hp = dot( T2, dot( G, dot(R, T1) ) )
+	####
+	# Now the first image
+	M = dot( Pp, dot( P.T, inv( dot(P, P.T) ) ) )
+	H0 = dot( Hp, M )
+	pts1h = apply_transformation_on_points(pts1, H0)
+	pts2h = apply_transformation_on_points(pts2, Hp)
+	# Construct inhomogeneous system
+	n = len(pts1)
+	A = zeros((n,3))
+	b = zeros((n,1))
+	for i in range(n):
+		xh,yh = pts1h[i][0], pts1h[i][1] 
+		xph = pts2h[i][0]
+		A[i] = [xh, yh, 1.]
+		b[i] = xph
+	# h is pseudo-inverse multiplied by b
+	h = dot( dot( inv( dot(A.T, A) ), A.T ), b )
+	h = h.flatten()
+	# Obtain the homography for the first image
+	HA = array([[h[0], h[1], h[2]], 
+			[0., 1., 0.],
+			[0., 0., 1.]])
+	H = dot(HA, H0)
+	return H, Hp
