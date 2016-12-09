69701da18011df9bbb1eb795e28068bed8f474ac
lifangda02@gmail.com <lifangda02@gmail.com>
2016-12-09 15:56:49 -0500

    adaboost template

--- HW11/adaboost.py
+++ HW11/adaboost.py
@@ -2,6 +2,31 @@
 from pylab import *
 import cv2
 
+WIDTH = 40
+HEIGHT = 20
+
+def _get_integral_image(image):
+	'''
+		Compute and return the integral representation of an gray-scale image.
+	'''
+	return cumsum(cumsum(image,axis=0),axis=1)
+
+def _define_features():
+	'''
+		Return a feature matrix where each row is a feature vector.
+	'''
+	# Do horizontal ones first
+	for s in range()
+
+def extract_features(data):
+	'''
+		Extract all features into a nFeatures x nSamples array.
+	'''
+	# Iterate through all features
+	# We are only concerned with two-rectangle edge features
+	# Get the sorted list of sample indices.
+	pass
+
 class CascadedAdaBoostClassifier(object):
 	def __init__(self):
 		super(CascadedAdaBoostClassifier, self).__init__()
@@ -15,8 +40,6 @@ class CascadedAdaBoostClassifier(object):
 			self.train_data = train_data
 			self.train_label = train_label
 			
-		def _train():
-			pass
 
 class AdaBoostClassifier(object):
 	def __init__(self):
